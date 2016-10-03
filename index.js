a8a5e7c939e3becea455173df0cd4596c586d2cf
Stephen Whitmore <stephen.whitmore@gmail.com>
2016-10-02 23:14:33 -0400

    use nodeback style

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ module.exports = function (cb) {
   if (process.argv.length <= 2) {
     // use stdin
     process.stdin.pipe(concat(function (buf) {
-      cb(buf.toString().trim())
+      cb(null, buf.toString().trim())
     }))
   } else {
     process.nextTick(function () {
-      cb(process.argv[2])
+      cb(null, process.argv[2])
     })
   }
 }
